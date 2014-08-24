678ad04196a8d7b6d739edf2badc15716d0c3e6d
walteryang <walteryang47@users.noreply.github.com>
2014-08-25 01:05:17 +0800

    Redirect to login page if user not signed in
    Signed-off-by: walteryang <walteryang47@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    if not user_signed_in?
+      redirect_to :controller => 'users', :action => :sign_in
+    else
+      throw exception
+    end
+  end
 end
