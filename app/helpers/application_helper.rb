2f0327cb705b9e509af8003731c8df1e1805405f
May <sirhuntthepure@users.noreply.github.com>
2014-09-02 17:47:25 +0800

    helpers: helper functions for using in views
    Signed-off-by: May <sirhuntthepure@users.noreply.github.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+  include DMHelper
+
+  def pri_display_selector
+    @ret[:primary_id][:name]
+  end
+
+  def pri_id_selector
+    @ret[:primary_id][:id]
+  end
+
+  def pri_display_helper
+    @ret[:primary][pri_display_selector]
+  end
+
+  def pri_id_helper
+    @ret[:primary][pri_id_selector]
+  end
+
+  def cur_display_selector
+    @ret[:current_id][:name]
+  end
+
+  def cur_id_selector
+    @ret[:current_id][:id]
+  end
+
+  def cur_display_helper
+    @ret[:current][cur_display_selector]
+  end
+
+  def cur_id_helper
+    @ret[:current][cur_id_selector]
+  end
 end
