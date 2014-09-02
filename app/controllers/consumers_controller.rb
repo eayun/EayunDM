af349989a18130fbf365b2669b336586c4e001b8
May <sirhuntthepure@users.noreply.github.com>
2014-09-02 18:24:53 +0800

    controllers: add consumer controller
    Signed-off-by: May <sirhuntthepure@users.noreply.github.com>

---
+++ app/controllers/consumers_controller.rb
@@ -0,0 +1,20 @@
+require "json"
+
+class ConsumersController < ApplicationController
+  def show
+    consumer = CANDLEPIN.execute "get_consumers", {:id => params[:id]}
+    @consumer = JSON.parse(consumer.to_str)
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  protected
+  def self.cur_id_f
+    {:id => "uuid", :name => "name"}
+  end
+
+  def self.pri_id_f
+    OwnersController::cur_id_f
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   end
   resources :contents
   resources :owners do
+    resources :consumers
     resources :subscriptions
   end
 
